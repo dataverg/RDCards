dd195ccf52bbeacc6e9324ea1ded268ab674d764
Obliv_SS <38468419+dataverg@users.noreply.github.com>
2024-06-27 09:20:00 -0500

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,105 @@
+let counter = 0;
+
+const counterValue = document.getElementById('counter-value');
+const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
+
+// To increment the value of counter
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    counterValue.innerHTML = counter;
+});
+
+// To decrement the value of counter
+decrementBtn.addEventListener('click', () => {
+    counter--;
+    counterValue.innerHTML = counter;
+});
+
+//============================================================================================//
+//=====================================COUNTER 2==============================================//
+//============================================================================================//
+
+let counter2 = 0;
+
+const counterValue2 = document.getElementById('counter-value2');
+const incrementBtn2 = document.getElementById('increment-btn2');
+const decrementBtn2 = document.getElementById('decrement-btn2');
+
+// To increment the value of counter
+incrementBtn2.addEventListener('click', () => {
+    counter2++;
+    counterValue2.innerHTML = counter2;
+});
+
+// To decrement the value of counter
+decrementBtn2.addEventListener('click', () => {
+    counter2--;
+    counterValue2.innerHTML = counter2;
+});
+
+//============================================================================================//
+//=====================================COUNTER 3==============================================//
+//============================================================================================//
+
+let counter3 = 0;
+
+const counterValue3 = document.getElementById('counter-value3');
+const incrementBtn3 = document.getElementById('increment-btn3');
+const decrementBtn3 = document.getElementById('decrement-btn3');
+
+// To increment the value of counter
+incrementBtn3.addEventListener('click', () => {
+    counter3++;
+    counterValue3.innerHTML = counter3;
+});
+
+// To decrement the value of counter
+decrementBtn3.addEventListener('click', () => {
+    counter3--;
+    counterValue3.innerHTML = counter3;
+});
+
+//============================================================================================//
+//=====================================COUNTER 4==============================================//
+//============================================================================================//
+
+let counter4 = 0;
+
+const counterValue4 = document.getElementById('counter-value4');
+const incrementBtn4 = document.getElementById('increment-btn4');
+const decrementBtn4 = document.getElementById('decrement-btn4');
+
+// To increment the value of counter
+incrementBtn4.addEventListener('click', () => {
+    counter4++;
+    counterValue4.innerHTML = counter4;
+});
+
+// To decrement the value of counter
+decrementBtn4.addEventListener('click', () => {
+    counter4--;
+    counterValue4.innerHTML = counter4;
+});
+
+//============================================================================================//
+//=====================================COUNTER 5==============================================//
+//============================================================================================//
+
+let counter5 = 0;
+
+const counterValue5 = document.getElementById('counter-value5');
+const incrementBtn5 = document.getElementById('increment-btn5');
+const decrementBtn5 = document.getElementById('decrement-btn5');
+
+// To increment the value of counter
+incrementBtn5.addEventListener('click', () => {
+    counter5++;
+    counterValue5.innerHTML = counter5;
+});
+
+// To decrement the value of counter
+decrementBtn5.addEventListener('click', () => {
+    counter5--;
+    counterValue5.innerHTML = counter5;
+});
